15194e06cc9f1c064b74dcb9eef2cdfe28174126
cocoaswifty <cocoaswifty@gmail.com>
2017-11-30 10:59:06 +0800

    Initial Commit

---
+++ YouNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YouNews
+//
+//  Created by Jianhao Huang on 2017/11/30.
+//  Copyright © 2017年 Jianhao Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
